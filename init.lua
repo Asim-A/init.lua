6b3b52965c6a3590302802a286450842c11c36f7
Asim Abazi <asimabazi@hotmail.no>
2024-04-14 16:39:32 +0200

    init

---
+++ init.lua
@@ -0,0 +1,4 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.opt.scrolloff = 30
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+local keymap = vim.keymap.set
+local builtin = require("telescope.builtin")
+
+keymap("i", "jk", "<Esc>")
+keymap("n", "<leader>ff", builtin.find_files, {})
+keymap("n", "<leader>fg", builtin.live_grep, {})
+keymap("n", "<leader>fb", builtin.buffers, {})
